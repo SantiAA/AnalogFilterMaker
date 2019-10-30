ac9ff206c33ee932fd3f374aebbd8777c8f9dce8
Nico <rtrozzo@itba.edu.ar>
2019-10-30 12:44:12 -0300

    Upgrades in stage2 interface

--- StagesManager/StagesManager.py
+++ StagesManager/StagesManager.py
@@ -1,7 +1,7 @@
 # python native modules
 from enum import Enum
 
-from numpy import conjugate
+from numpy import conjugate, amax
 from numpy import argmin
 from numpy import full
 from numpy import log10
@@ -9,22 +9,24 @@ from numpy import log10
 
 # AFM project modules
 from Filters.Filters import Filter
+from StagesManager.Pole import Pole
+from StagesManager.Zero import Zero
 
 
 class StagesManager(object):
 
     def __init__(self, fil: Filter):
-        self.p_pairs = [] # va a tener arreglos de polos de 1er o 2do orden, guarda wo y Q (Q solo en 2do)
-        self.z_pairs = [] # va a tener arreglos de ceros de 1er y 2do orden, guarda wo y orden (siempre estan sobre el eje im)
+        self.p_pairs = [] # va a tener arreglo de Poles
+        self.z_pairs = [] # va a tener arreglos de Zeros
         self.sos = []
         """ Guarda todos los polos y ceros agrupados en etapas de 1/2do orden """
-        z, p, self.k, q = fil.get_z_p_k_q()
+        z, p, self.k_tot, q = fil.get_z_p_k_q()
         saved = False
         while len(p):  # guardo en self.p_pairs los pares de polos complejos conjugados como [wo,Q]
             if len(p) > 1:
                 if p[0] == conjugate(p[1]):
-                    self.p_pairs.append([abs(p[0]), q[0]])
-                    q.remove(q[0])
+                    #self.p_pairs.append([abs(p[0]), q[0]])
+                    self.p_pairs.append(Pole(p))
                     p.remove(p[1])
                     saved = True
             if not saved:
@@ -34,24 +36,15 @@ class StagesManager(object):
         while len(z):  # guardo en self.z_pairs los pares de ceros complejos conjugados como [wo,n]
             if len(z) > 1:
                 if z[0] == conjugate(z[1]):
-                    self.z_pairs.append([abs(z[0]), 2])
+                    self.z_pairs.append(Zero(abs(z[0].im), 2))
                     z.remove(z[1])
                     saved = True
             if not saved:
                 self.z_pairs.append([abs(z[0])])  # si no tiene
             z.remove(z[0])
             saved = False
-        self.p_pairs.sort(key=lambda x: x[1], reverse=True)  # ordeno polos por Q decreciente
-        self.z_pairs.sort(key=lambda x: x[1], reverse=True)  # ordeno ceros por orden decreciente
-        self.sos = []
-
-
-    def load_filter(self, fil: Filter):
-
-
-    def get_singularities(self):
-        """ Returns tuple with 1st/2nd order poles and 1st/2nd order zeros """
-        return self.z_pairs, self.p_pairs
+        self.p_pairs.sort(key=lambda x: x.q, reverse=True)  # ordeno polos por Q decreciente
+        self.z_pairs.sort(key=lambda x: x.n, reverse=True)  # ordeno ceros por orden decreciente
 
     def auto_max_rd(self, vi_min, vi_max):
         # agrupo todas
@@ -87,8 +80,12 @@ class StagesManager(object):
             pass
         return self.sos
 
-    def add_stage(self, p: Pole, z: Zero) -> (bool,str):
-        """ Devuelve True si ya estan cargadas todas las etapas, False si no """
+    def get_stages(self):
+        """" Returns Stages list """
+        return self.sos
+
+    def add_stage(self, p_str: str, z_str: str) -> (bool,str):
+        """ Devuelve True es valida la etapa solicitada, False si no """
         # self.sos[i]["Poles"] = p
         # self.sos[i]["Zeros"] = z
 
@@ -98,55 +95,85 @@ class StagesManager(object):
 
     def delete_stages(self, indexes: list):
         """" Deletes stages indicated by indexes list """
-        pass
+        if amax(indexes) < len(self.sos):
+            self.sos.remove(self.sos[indexes])
+        else:
+            print("Indexes list out of range!")
 
     def calc_rd(self):
         # vi_max = self.requirements[StageInfo.Vo_max]
         # for s in self.sos:
         #     vi_max /= s["Gain"]
-        # rd = 20*log10(vi_max/self.requirements[StageInfo.Vi_min])
-
+        # rd = 20*log10(vi_max/self.requirements[StageInfo.Vi_min]
         return rd
 
-    def set_gain(self, i, k):
-        self.sos[i]["Gain"] = k
-
+    def set_gain(self, i, k) -> (bool, str):
+        if i < len(self.sos):
+            partial_gain = 0
+            for k in range(len(self.sos)):
+                partial_gain += self.sos[k].k if k != i else k
+            if partial_gain <= self.k_tot:
+                self.sos[i].k = k
+                return True, ""
+            else:
+                return False, "Total gain can't exceed" + str(self.k_tot) + "dB"
+        else:
+            return False, "Stage" + str(i + 1) + "doesn't exist."
     def get_z_p_plot(self):
         """" Returns poles and zeros diagram with number of repeticiones of each pole and zero """
+
+
+# i = 0
+# while i < len(self.denormalized["Zeros"]):
+#     count = self.denormalized["Zeros"].count(self.denormalized["Zeros"][i])
+#     repeated_z.append(count)
+#     z.append(self.denormalized["Zeros"][i])
+#     i += count
+# i = 0
+# while i < len(self.denormalized["Poles"]):
+#     count = self.denormalized["Poles"].count(self.denormalized["Poles"][i])
+#     repeated_p.append(count)
+#     p.append(self.denormalized["Poles"][i])
+#     i += count
+
+# graphs[GraphTypes.PolesZeros.value] = [[GraphValues(real(z),
+#                                                     imag(z), True, False, False,
+#                                                     "Zeros", repeated_z),
+#                                         GraphValues(real(p),
+#                                                     imag(p), True, True, False,
+#                                                     "Poles", repeated_p)], ["Re(s)[rad/sec]", "Im(s)[rad/sec]"]]
+
 
     def get_z_p_dict(self):
         """ Returns a dictionary with all zeros and poles:
          { "Poles": {"1st order": [Poles], "2nd order": [Poles]}
            "Zeros": {"1st order": [Zeros], "2nd order": [Zeros]} } """
+        ret = {"Poles": {"1st order": [], "2nd order": []}, "Zeros": {"1st order": [], "2nd order": []}}
+        for p in self.p_pairs:
+            key2 = "1st order" if p.q < 0 else "2nd order"
+            ret["Poles"][key2].append(p)
+        for z in self.z_pairs:
+            key2 = "1st order" if z.n == 1 else "2nd order"
+            ret["Zeros"][key2].append(z)
+        return ret
 
-        # i = 0
-        # while i < len(self.denormalized["Zeros"]):
-        #     count = self.denormalized["Zeros"].count(self.denormalized["Zeros"][i])
-        #     repeated_z.append(count)
-        #     z.append(self.denormalized["Zeros"][i])
-        #     i += count
-        # i = 0
-        # while i < len(self.denormalized["Poles"]):
-        #     count = self.denormalized["Poles"].count(self.denormalized["Poles"][i])
-        #     repeated_p.append(count)
-        #     p.append(self.denormalized["Poles"][i])
-        #     i += count
-
-        # graphs[GraphTypes.PolesZeros.value] = [[GraphValues(real(z),
-        #                                                     imag(z), True, False, False,
-        #                                                     "Zeros", repeated_z),
-        #                                         GraphValues(real(p),
-        #                                                     imag(p), True, True, False,
-        #                                                     "Poles", repeated_p)], ["Re(s)[rad/sec]", "Im(s)[rad/sec]"]]
     def get_stages_plot(self):
-        pass
+        plot_list = [[],[]]
+        for st in self.sos:
+            plot_list[0].append(st.get_tf_plot())
+        plot_list[1] = ["Frequency [Hz]", "Amplitude [dB]"]
+        return plot_list
 
-
-    def get_dr(self),vi_min, vi_max):
+    def get_dr(self, vi_min, vi_max):
         """Returns a tuple:
             (True, dr) if everytihing ok, (False, err_str) if error
             Could fail because of: Invalid vi values, or not all stages loaded"""
-        pass
+        for st in self.sos:
+
+
+
+    def _get_stg_dr(self, i, vi_min, vi_max):
+
 
     def get_const_data(self, i):
         """Returns a dictionary with string values of the stage i"""
